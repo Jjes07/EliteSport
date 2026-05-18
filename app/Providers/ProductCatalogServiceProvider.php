b56655b5a3671b953e7651e3dac319a442aa8aac
Jjes07 <jjescobars@eafit.edu.co>
2026-05-17 20:37:49 -0500

    Testing FakeProductCatalog

--- app/Providers/ProductCatalogServiceProvider.php
+++ app/Providers/ProductCatalogServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Interfaces\ProductCatalog;
 use App\Utils\DatabaseProductCatalog;
+use APP\Utils\FakeProductCatalog;
 use Illuminate\Support\ServiceProvider;
 
 class ProductCatalogServiceProvider extends ServiceProvider
@@ -11,7 +12,7 @@ class ProductCatalogServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->bind(ProductCatalog::class, function () {
-            return new DatabaseProductCatalog;
+            return new FakeProductCatalog;
         });
     }
 }
